acd36ada21e62eac813033f40682404cdb900831
Wang Ruqin <wangruqin35@gmail.com>
2025-04-22 11:15:44 +0900

    add coment

--- run.sh
+++ run.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+# 遍历文件夹中的所有文件
 # フォルダ内のすべてのファイルに対してループ
+# Loop through all files in the folder
 for FILE in configs/*; do
+    # 对每个文件运行 Python 脚本
     # 各ファイルに対してPythonスクリプトを実行
-    python main.py -config_path "$FILE"
+    # Run the Python script for each file
+    python main.py -config_path "requirements.txt"
 done
